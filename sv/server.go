7de998560980ba5034cc62c1bf0db4bcac418d8c
LeeRubiin <nazgul.smailova03@mail.ru>
2023-06-01 11:25:35 +0600

    add Pomodoro Timer

---
+++ sv/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+    "net/http"
+
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "pomidor.html")
+	})
+
+	/*// Обработчик для CSS файлов
+	http.HandleFunc("/styles.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "styles.css")
+	})*/
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8282", nil)
+}
+
+
